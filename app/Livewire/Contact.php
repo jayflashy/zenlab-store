6559450322ed5bc4d0883ac884d2dafb81dbe679
Joshua Adeyemi <joshuaadedolapo@gmail.com>
2025-04-10 11:30:44 +0100

    contact us and breadcrumb component

---
+++ app/Livewire/Contact.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Livewire;
+
+use App\Traits\LivewireToast;
+use Livewire\Component;
+
+class Contact extends Component
+{
+    use LivewireToast;
+    public function render()
+    {
+        return view('livewire.contact');
+    }
+}
---
+++ app/View/Components/Breadcrumb.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\View\Components;
+
+use Illuminate\View\Component;
+
+class Breadcrumb extends Component
+{
+    public $title;
+    public $page;
+
+    public function __construct($title, $page)
+    {
+        $this->title = $title;
+        $this->page = $page;
+    }
+
+    public function render()
+    {
+        return view('components.breadcrumb');
+    }
+}
