164eea3afd23aa8ad9eb136f49e96dffe9af0cd4
Joshua Adeyemi <joshuaadedolapo@gmail.com>
2025-04-15 08:04:22 +0100

    updates rector config

--- rector.php
+++ rector.php
@@ -2,7 +2,12 @@
 
 declare(strict_types=1);
 
+use Rector\CodeQuality\Rector\Class_\InlineConstructorDefaultToPropertyRector;
 use Rector\Config\RectorConfig;
+use Rector\Set\ValueObject\SetList;
+use Rector\TypeDeclaration\Rector\Property\TypedPropertyFromStrictConstructorRector;
+use RectorLaravel\Set\LaravelLevelSetList;
+use RectorLaravel\Set\LaravelSetList;
 
 return RectorConfig::configure()
     ->withPaths([
@@ -13,8 +18,32 @@ return RectorConfig::configure()
         __DIR__ . '/routes',
         __DIR__ . '/tests',
     ])
+    ->withSkip([
+        __DIR__ . '/bootstrap/cache/*',
+        // Skip Laravel-generated views
+        // __DIR__ . '/resources/views/*',
+        \RectorLaravel\Rector\ClassMethod\ScopeNamedClassMethodToScopeAttributedClassMethodRector::class,
+
+    ])
     // uncomment to reach your current PHP version
     ->withPhpSets()
-    ->withTypeCoverageLevel(0)
-    ->withDeadCodeLevel(0)
-    ->withCodeQualityLevel(0);
+    ->withSets([
+
+        LaravelLevelSetList::UP_TO_LARAVEL_120,
+        LaravelSetList::LARAVEL_CODE_QUALITY,
+
+        SetList::NAMING,
+        SetList::CODING_STYLE,        // Changes coding style
+        SetList::DEAD_CODE,             // Removes unused code
+        SetList::EARLY_RETURN,          // Converts nested conditions to early returns
+        SetList::TYPE_DECLARATION,
+    ])
+    ->withTypeCoverageLevel(5)
+    ->withDeadCodeLevel(5)
+    ->withCodeQualityLevel(5)
+    ->withRules([
+        InlineConstructorDefaultToPropertyRector::class,
+        TypedPropertyFromStrictConstructorRector::class,
+    ])
+    // Parallel processing for faster execution
+    ->withParallel();;
