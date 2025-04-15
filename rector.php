3b8b8dcb9f07f423f9fa8da30a098c3521c90fbb
Joshua Adeyemi <joshuaadedolapo@gmail.com>
2025-04-15 07:36:25 +0100

    phpstan and rector config

--- rector.php
+++ rector.php
@@ -9,13 +9,12 @@ return RectorConfig::configure()
         __DIR__ . '/app',
         __DIR__ . '/bootstrap',
         __DIR__ . '/config',
-        __DIR__ . '/public',
         __DIR__ . '/resources',
         __DIR__ . '/routes',
         __DIR__ . '/tests',
     ])
     // uncomment to reach your current PHP version
-    // ->withPhpSets()
+    ->withPhpSets()
     ->withTypeCoverageLevel(0)
     ->withDeadCodeLevel(0)
     ->withCodeQualityLevel(0);
