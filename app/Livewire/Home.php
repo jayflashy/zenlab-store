914e132f8ff03744be76949f3feb1aa826f136e3
jayflashy <109805121+jayflashy@users.noreply.github.com>
2025-04-24 08:06:04 +0000

    fix code style

--- app/Livewire/Home.php
+++ app/Livewire/Home.php
@@ -8,9 +8,11 @@ use Livewire\Component;
 class Home extends Component
 {
     public $blogs;
+
     public function render()
     {
         $this->blogs = Blog::where('is_active', 1)->latest()->take(3)->get();
+
         return view('livewire.home');
     }
 }
