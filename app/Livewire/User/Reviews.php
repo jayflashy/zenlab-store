8e03110c61760085ce5aad65b1bddaec41a3c205
jayflashy <109805121+jayflashy@users.noreply.github.com>
2025-04-30 07:40:27 +0000

    fix code style

--- app/Livewire/User/Reviews.php
+++ app/Livewire/User/Reviews.php
@@ -3,13 +3,14 @@
 namespace App\Livewire\User;
 
 use App\Traits\LivewireToast;
-use Livewire\Component;
 use Livewire\Attributes\Layout;
+use Livewire\Component;
 
 #[Layout('user.layouts.app')]
 class Reviews extends Component
 {
     use LivewireToast;
+
     public function render()
     {
         return view('livewire.user.reviews');
